fafd389c49c03755669c8bd624afb522fa627c9e
Petra Heřmánková <petra.janouchova@gmail.com>
2021-08-09 14:03:21 +0200

    updated text

--- src/dashboards/model_explorer.py
+++ src/dashboards/model_explorer.py
@@ -33,7 +33,7 @@ with open('mdl/interpretive_oversampled/210602_preprocessing.pcl', 'rb') as fin:
 st.set_page_config(
     page_icon='🏛',
     layout='centered',
-    page_title='Ancient Classifier'
+    page_title='Epigraphic Classifier for Latin inscriptions'
 )
 
 # ===
@@ -43,36 +43,38 @@ with st.sidebar:
     # track which model to use 
     model_choice = st.selectbox(
         'Which model do you want to use?',
-        ('As-is on inscriptions (conservative text)', 'Full sentences (interpretive text)')
+        ('Text as-is on inscriptions (conservative text)', 'Reconstructed text (interpretive text)')
     )
 
-    if model_choice == 'As-is on inscriptions (conservative text)':
+    if model_choice == 'Text as-is on inscriptions (conservative text)':
         default_input = 'Accae l Myrine Accae l Sympherusae M Ant M l Ero poma'
 
-    elif model_choice == 'Full sentences (interpretive text)':
+    elif model_choice == 'Reconstructed text (interpretive text)':
         default_input = 'Accae mulieris libertae Myrine Accae mulieris libertae Sympherusae Marco Antonio Marci liberto Ero pomario'
 
 
 # ===
 # Classify input
 # ===
-st.title('Ancient Classifier')
-st.write('Label ancient Mediterranean roadside inscriptions. [Source code](https://github.com/centre-for-humanities-computing/ancient-classifier/), forked from the [Epigraphic Roads](https://github.com/sdam-au/epigraphic_roads/) project.')
+st.title('Epigraphic Classifier for Latin inscriptions')
+st.write('This model assigns standard typologies of inscriptions to a given Latin text and shows the confidence of assigned classifications. The model was trained on inscriptions from the [Epigraphic Database Heidelberg](https://edh-www.adw.uni-heidelberg.de/).[Source code](https://github.com/centre-for-humanities-computing/ancient-classifier/), forked from the [Epigraphic Roads](https://github.com/sdam-au/epigraphic_roads/) project.')
 
-user_input = st.text_area("Input text", default_input)
+st.write('Authors: Petra Hermankova & Jan Kostkan, Aarhus University')
+
+user_input = st.text_area("Insert text", default_input)
 
 if not isinstance(user_input, str):
-    raise TypeError('Input not string')
+    raise TypeError('Input is not a string')
 
 if len(user_input) > 1000:
     raise MemoryError('Input text too long. Max 1000 characters allowed')
 
 if st.button('Classify!'):
 
-    if model_choice == 'As-is on inscriptions (conservative text)':
+    if model_choice == 'Text as-is on inscriptions (conservative text)':
         transformer = mdl_prep_conservative
         model = mdl_clf_conservative
-    elif model_choice == 'Full sentences (interpretive text)':
+    elif model_choice == 'Reconstructed text (interpretive text)':
         transformer = mdl_prep_interpretive
         model = mdl_clf_interpretive
 
@@ -89,7 +91,7 @@ if st.button('Classify!'):
     st.code(f'{y_pred}')
     st.write('\n\n')
 
-    st.subheader('Confidence')
+    st.subheader('Confidence values')
     st.write('\n')
     st.write(
         plot_bar_confidence(confidence_df)
